54c36358f3ff208b84f59c3ede4ca31ae7a743c3
thanhhai <haint@mpos.vn>
2022-05-09 15:07:08 +0700

    update

--- src/.env.js
+++ src/.env.js
@@ -1,9 +1,6 @@
 export const API_URL = process.env.VUE_APP_API_URL || 'localhost'
 export const GET_ENDPOINT_URL = process.env.VUE_APP_GET_ENDPOINT_URL || 'localhost'
 export const GET_SYSTEM_CONFIG_URL = process.env.VUE_APP_GET_SYSTEM_CONFIG_URL || 'localhost'
-export const GET_SYSTEM_CONFIG_CCRP = process.env.VUE_APP_GET_CCRP_CONFIG || 'localhost'
 export const DOMAIN_TITLE = process.env.VUE_APP_DOMAIN_TITLE || 'IMPEL'
-// export const GOOGLE_API_TOKEN = process.env.VUE_APP_GOOGLE_API_TOKEN || 'GOOGLE_API_TOKEN'
-export const GOOGLE_API_TOKEN = 'AIzaSyAhD3CQeL6_BsPHibHrCu-JPL8mxGwCH0Q' || 'GOOGLE_API_TOKEN'
 export const TOKEN_INSIDE = process.env.VUE_APP_INSIDE_TOKEN || 'TOKEN_INSIDE'
 
