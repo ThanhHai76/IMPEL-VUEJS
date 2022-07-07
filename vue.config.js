0e1636bbcc4db46090a8b089f4f143655e3544ac
thanhhai <haint@mpos.vn>
2022-07-07 18:23:57 +0700

    update

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,7 @@
 // https://cli.vuejs.org/guide/css.html#passing-options-to-pre-processor-loaders
 module.exports = {
   lintOnSave: false,
-  css: {
-    loaderOptions: {
-      scss: {
-        prependData: `
-          @import "@/scss/_variables.scss";
-          @import "@/scss/_fonts.scss";
-          @import "@/scss/_medias.scss";
-          @import "@/scss/_mixins.scss";
-        `
-      }
-    }
-  },
+  css: {},
 
   devServer: {
     disableHostCheck: true,
